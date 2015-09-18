3a0d9a278a3551d6c2eae71429ccb0ef3002eefd
Neil <neil@Neils-MacBook-Pro.local>
2015-09-18 10:26:32 -0700

    Having specs folder and executing in parallel

--- conf.js
+++ conf.js
@@ -4,7 +4,7 @@ exports.config = {
   sauceKey: process.env.SAUCE_ACCESS_KEY,
 
   // seleniumAddress: 'http://ondemand.saucelabs.com:80/wd/hub',
-  specs: ['*spec.js'],
+  specs: ['specs/*spec.js'],
 
   // restartBrowserBetweenTests: true,
 
@@ -14,14 +14,14 @@ exports.config = {
     version: '32',
     platform: 'OS X 10.10',
     shardTestFiles: true,
-    name: "firefox",
+    name: "firefox-tests",
     maxInstances: 10
   }, {
     browserName: 'chrome',
     version: '41',
     platform: 'Windows 7',
     shardTestFiles: true,
-    name: "chrome",
+    name: "chrome-tests",
     maxInstances: 10
   }]
 }
