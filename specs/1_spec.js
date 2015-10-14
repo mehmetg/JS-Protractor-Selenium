38446d06d696356cfb4a10d2224101dc81e59313
Kevin Berg <kevinberg@Kevins-MacBook-Pro.local>
2015-10-14 11:37:47 -0700

    Add working test

--- specs/1_spec.js
+++ specs/1_spec.js
@@ -1,8 +1,12 @@
 // spec.js
 describe('Protractor Demo App1', function() {
-  it('should have a title', function() {
-    browser.get('http://yahoo.com');
+  it('should greet the named user', function() {
+    browser.get('http://www.angularjs.org');
 
-    expect(browser.getTitle()).toEqual('Yahoo');
+    element(by.model('yourName')).sendKeys('Example');
+
+    var greeting = element(by.binding('yourName'));
+
+    expect(greeting.getText()).toEqual('Hello Example!');
   });
 });
