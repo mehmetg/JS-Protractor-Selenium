400f20dad75c7bc290a9f90601425f28f93c2029
Mehmet Gerceker <mehmetg@saucelabs.com>
2015-11-30 16:49:06 -0800

    Making REST Api use the relay tunnel on SC.

--- conf.js
+++ conf.js
@@ -3,7 +3,7 @@
 var HttpsProxyAgent = require("https-proxy-agent");
 
 var sauceConnectRelayPort = process.env.SELENIUM_PORT;
-var agent = new HttpsProxyAgent('https://localhost:56193');
+var agent = new HttpsProxyAgent('https://localhost:' + sauceConnectRelayPort);
 exports.config = {
 
     sauceSeleniumAddress: 'localhost:' + sauceConnectRelayPort + '/wd/hub',
