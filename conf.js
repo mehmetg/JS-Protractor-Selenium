b966f8cc382a0539c7384991e9baa3e756f04fed
Mehmet Gerceker <mehmetg@saucelabs.com>
2015-12-02 16:59:41 -0800

    Revert proxy changes. x3

--- conf.js
+++ conf.js
@@ -15,33 +15,33 @@ exports.config = {
 
     restartBrowserBetweenTests: true,
 
-    onPrepare: function(){
-    var caps = browser.getCapabilities()
-},
+    onPrepare: function () {
+        var caps = browser.getCapabilities()
+    },
 
-multiCapabilities: [{
-    browserName: 'firefox',
-    version: '32',
-    platform: 'OS X 10.10',
-    name: "firefox-tests",
-    shardTestFiles: true,
-    maxInstances: 25
-}, {
-    browserName: 'chrome',
-    version: '41',
-    platform: 'Windows 7',
-    name: "chrome-tests",
-    shardTestFiles: true,
-    maxInstances: 25
-}],
+    multiCapabilities: [{
+        browserName: 'firefox',
+        version: '32',
+        platform: 'OS X 10.10',
+        name: "firefox-tests",
+        shardTestFiles: true,
+        maxInstances: 25
+    }, {
+        browserName: 'chrome',
+        version: '41',
+        platform: 'Windows 7',
+        name: "chrome-tests",
+        shardTestFiles: true,
+        maxInstances: 25
+    }],
 
     onComplete: function () {
 
         var printSessionId = function (jobName) {
             browser.getSession().then(function (session) {
                 console.log('SauceOnDemandSessionID=' + session.getId() + ' job-name=' + jobName);
-            }
-            printSessionId(process.env.BUILD_TAG);
+            });
         }
+        printSessionId(process.env.BUILD_TAG);
     }
 };
