35c968b46b3295deed8a36ac7c52e71521bcd812
Mehmet Gerceker <mehmetg@msn.com>
2016-06-24 12:55:31 -0700

    Adda build tag.

--- conf.js
+++ conf.js
@@ -1,8 +1,16 @@
 // conf.js
+var jasmineReporters = require('jasmine-reporters');
+var HttpsProxyAgent = require('https-proxy-agent');
+var q = require('q');
+
+const PROXY_URL_WITH_PORT = "http://phxappgwe2-vip.phx.aexp.com:9090";
+var agent = new HttpsProxyAgent(PROXY_URL_WITH_PORT);
+
 exports.config = {
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
-
+  sauceAgent: agent,
+  webDriverProxy: PROXY_URL_WITH_PORT
   //seleniumAddress: 'http://ondemand.saucelabs.com:80/wd/hub',
   specs: ['specs/*spec.js'],
 
@@ -12,29 +20,48 @@ exports.config = {
       var caps = browser.getCapabilities()
   },
 
-  multiCapabilities: [{
-    browserName: 'firefox',
-    version: '32',
-    platform: 'OS X 10.10',
-    name: "firefox-tests",
-    shardTestFiles: true,
-    maxInstances: 25
-  }, {
-    browserName: 'chrome',
-    version: '41',
-    platform: 'Windows 7',
-    name: "chrome-tests",
-    shardTestFiles: true,
-    maxInstances: 25
-  }],
+  framework: 'jasmine2',
 
-  onComplete: function() {
+  getMultiCapabilities: function () {
+      var deferred = q.defer();
+      var multiCaps = [{
+          browserName: 'firefox',
+          version: '32',
+          platform: 'OS X 10.10',
+          name: "firefox-tests",
+          shardTestFiles: true,
+          maxInstances: 25
+      }, {
+          browserName: 'chrome',
+          version: '41',
+          platform: 'Windows 7',
+          name: "chrome-tests",
+          shardTestFiles: true,
+          maxInstances: 25
+      }];
+      for (var i = 0; i < multiCaps.length; i++) {
+          multiCaps[i].build = process.env.BUILD_TAG;
+      }
+      deferred.resolve(multiCaps);
+      return deferred.promise;
+  },
 
-    var printSessionId = function(jobName){
-      browser.getSession().then(function(session) {
-        console.log('SauceOnDemandSessionID=' + session.getId() + ' job-name=' + jobName);
+  onPrepare: function() {
+      jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+          consolidateAll: true,
+          savePath: 'testresults',
+          filePrefix: 'xmloutput'
+      }));
+  },
+
+  onComplete: function () {
+      browser.getSession().then(function (session) {
+        return browser.getProcessedConfig().then(function (config) {
+              // config.capabilities is the CURRENT capability being run, if
+              // you are using multiCapabilities.
+              console.log('SauceOnDemandSessionID=' + session.getId() + ' job-name=' + config.capabilities.name);
+              return browser.get
+          });
       });
-    }
-    printSessionId("Insert Job Name Here");
   }
 }
